1a6306888d8bc5bd4f1df8ed3ee39bf4b3ad34ab
kolosya-tuchka <74771359+kolosya-tuchka@users.noreply.github.com>
2021-11-19 13:56:12 +0700

    Added some functions that crete and add attributes to products.

--- includes/class-smart-sorting-activator.php
+++ includes/class-smart-sorting-activator.php
@@ -29,27 +29,79 @@ class Smart_Sorting_Activator {
 	 *
 	 * @since    1.0.0
 	 */
-	public static function activate() {
-        global $wp_query;
-        if($wp_query->have_posts()){
-            global $post;
-            while ($wp_query->have_posts()){
-                $wp_query->the_post();
-                $product = new WC_Product($post->ID);
-                $val = rand(1, 100);
-                $product->set_attributes(array(
-                    'id' => 0,
-                    'name' => 'spv',
-                    'options' => $val,
-                    'position' => $val,
-                    'visible' => true,
-                    'variation' => true
-                ));
-                $product->save();
-            }
 
+    /**
+     *
+     * @param string name
+     *
+     * @return WC_Product_Attribute/bool
+     *
+     */
+
+    static function make_attribute($name){
+        global $wpdb;
+
+        $insert = $wpdb->insert(
+            $wpdb->prefix . 'woocommerce_attribute_taxonomies',
+            array(
+                'attribute_label' => $name,
+                'attribute_name' => $name,
+                'attribute_type' => 'select',
+                'attribute_orderby' => 'menu_order',
+                'attribute_public' => 1
+            ),
+            array( '%s', '%s', '%s', '%s', '%d')
+        );
+
+        delete_transient('wc_attribute_taxonomies');
+    }
+
+    static function add_spv_attribute($name) {
+        global $wc_product_attributes;
+
+        if (!isset($wc_product_attributes[$name])) {
+            self::make_attribute($name);
         }
 
-	}
+        $attr = new WC_Product_Attribute();
+        $attr->set_id(1);
+        $attr->set_name($name);
+        $attr->set_visible(true);
+        $attr->set_variation(false);
+        $attr->set_value(0);
+        return $attr;
+    }
 
+    public static function add_def_attributes($product, $attributes) {
+        $spv_attributes = array(
+            'spv_views',
+            'spv_sales',
+            'spv',
+        );
+
+        foreach ($spv_attributes as $key) {
+            if (!isset($attributes[$key])){
+                $attr = self::add_spv_attribute($key);
+                if ($attr) {
+                    $attributes[$key] = $attr;
+                }
+            }
+        }
+        $product->set_attributes($attributes);
+        $product->save();
+    }
+
+	public static function activate() {
+        $product_query = new WP_Query( array(
+           'post_type' => 'product',
+        ));
+        if($product_query->have_posts()){
+            while ($product_query->have_posts()){
+                $post = $product_query->the_post();
+                $product = WC_Product($post->ID);
+                $attributes = $product->get_attributes();
+                self::add_def_attributes($product, $attributes);
+            }
+        }
+	}
 }
