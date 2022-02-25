d46a023031d97383f92db6df8aceaf3c9ebec750
Bit7eg <stonysyrop@gmail.com>
2022-02-25 18:24:40 +0700

    Added validation by product category

--- public/class-smart-sorting-public.php
+++ public/class-smart-sorting-public.php
@@ -147,15 +147,24 @@ class Smart_Sorting_Public {
                             $product_id
                         )
                     );
-                    $view_nums = $wpdb->get_col(
+                    $view_nums = $wpdb->get_result(
                         $wpdb->prepare(
-                            "SELECT view_num FROM `wp_smart-sorting_views_table` WHERE view_date > DATE_SUB(CURRENT_DATE, INTERVAL 7 DAY)"
+                            "SELECT product_id, view_num FROM `wp_smart-sorting_views_table` WHERE view_date > DATE_SUB(CURRENT_DATE, INTERVAL 7 DAY)"
                         )
                     );
+                    $sale_terms = get_the_terms($product_id, 'product_cat');
+                    $sum = 0;
+                    foreach ($view_nums as $view_num) {
+                        foreach (get_the_terms($view_num->product_id, 'product_cat') as $view_term) {
+                            if (in_array($view_term, $sale_terms)) {
+                                $sum += $view_num->view_num;
+                            }
+                        }
+                    }
                     $wpdb->query(
                         $wpdb->prepare(
                             "UPDATE {$wpdb->postmeta} SET meta_value = meta_value + %d WHERE post_id = %d AND meta_key='spv_views'",
-                            array_sum($view_nums),
+                            $sum,
                             $product_id
                         )
                     );
