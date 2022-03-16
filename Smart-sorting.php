713adf8d283dde7b7bd4032e6546647147dd158a
Bit7eg <stonysyrop@gmail.com>
2022-03-16 20:32:03 +0700

    Added license

--- Smart-sorting.php
+++ Smart-sorting.php
@@ -13,8 +13,8 @@
  * Version:           1.0.0
  * Author:            SmartSorting
  * Author URI:        http://example.com/
- * License:           GPL-2.0+
- * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * License:           LGPL-3.0+
+ * License URI:       https://www.gnu.org/licenses/lgpl-3.0.txt
  * Text Domain:       smart-sorting
  * Domain Path:       /languages
  */
