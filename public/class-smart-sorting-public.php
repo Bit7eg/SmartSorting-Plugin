aeb3db1fcb4c4c7501f9533e63ff2302c3892368
Bit7eg <stonysyrop@gmail.com>
2022-06-28 20:24:21 +0700

    Added function which process ajax request. Added object description for script to the enqueue_scripts function

--- public/class-smart-sorting-public.php
+++ public/class-smart-sorting-public.php
@@ -59,9 +59,8 @@ class Smart_Sorting_Public {
 	 * @since    0.1.0
 	 */
 	public function enqueue_styles() {
-
 		wp_enqueue_style(
-            $this->smart_sorting,
+            $this->smart_sorting . '_public_style',
             plugin_dir_url( __FILE__ ) . 'css/smart-sorting-public.css',
             array(),
             $this->version,
@@ -76,15 +75,23 @@ class Smart_Sorting_Public {
 	 * @since    0.1.0
 	 */
 	public function enqueue_scripts() {
-
 		wp_enqueue_script(
-            $this->smart_sorting,
+            $this->smart_sorting . '_public_script',
             plugin_dir_url( __FILE__ ) . 'js/smart-sorting-public.js',
             array( 'jquery' ),
             $this->version,
             false
         );
 
+        wp_localize_script(
+            $this->smart_sorting . '_public_script',
+            'ajax_obj',
+            array(
+                'ajax_url' => plugin_dir_url( dirname(__FILE__) ) .
+                    'includes/class-smart-sorting.php',
+                'nonce'    => wp_create_nonce( 'increment_view' ),
+            )
+        );
 	}
 
     /**
@@ -107,6 +114,51 @@ class Smart_Sorting_Public {
             $args['meta_key'] = 'spv';
         }
         return $args;
+    }
+
+    /**
+     * Add the current view to the database.
+     *
+     * @since   0.1.0
+     */
+    public static function ajax_add_view() {
+        global $wpdb;
+
+        check_ajax_referer( 'increment_view' );
+        $product_id = $_POST['productId'];
+
+        if ( is_user_logged_in() ) {
+            $user_id = get_current_user_id();
+        } else {
+            $user_id = -1;
+        }
+
+        $wpdb->query(
+            $wpdb->prepare(
+                "UPDATE wp_smart_sorting_views_table
+            SET view_num = view_num + 1
+            WHERE product_id = %d
+              AND user_id = %d
+              AND view_date = CURRENT_DATE
+              AND is_counted = 0",
+                $product_id,
+                $user_id,
+            )
+        );
+
+        if ( 0 == $wpdb->rows_affected ) {
+            $wpdb->query(
+                $wpdb->prepare(
+                    "INSERT INTO wp_smart_sorting_views_table
+                (product_id, user_id, view_date, view_num)
+                VALUES (%d, %d, CURRENT_DATE, 1)",
+                    $product_id,
+                    $user_id,
+                )
+            );
+        }
+
+        wp_die();
     }
 
     /**
